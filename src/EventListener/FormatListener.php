a2094aaf292506fad02001f948ef7b72f9fd4494
Alexandr Zolotukhin <supersmile2009@gmail.com>
2020-03-17 15:44:13 +0200

    PNP-23148 Set current request format from content-type header

---
+++ src/EventListener/FormatListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ugm\SwooleGrpc\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class FormatListener implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        $format = $request->getPreferredFormat(null);
+        if (null !== $format) {
+            return;
+        }
+
+        $request->setRequestFormat($request->getContentType());
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => ['onKernelRequest', 34],
+        ];
+    }
+}
