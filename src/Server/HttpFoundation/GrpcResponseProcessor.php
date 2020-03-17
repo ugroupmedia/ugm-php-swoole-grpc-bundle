bb92261b3b77c802c55dcdb8f26839ca42fbbb94
Alexandr Zolotukhin <supersmile2009@gmail.com>
2020-03-17 15:45:07 +0200

    PNP-23148 Send trailers with gRPC status and error details

--- src/Server/HttpFoundation/GrpcResponseProcessor.php
+++ src/Server/HttpFoundation/GrpcResponseProcessor.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Ugm\SwooleGrpc\Server\HttpFoundation;
 
+use ErrorException;
 use K911\Swoole\Bridge\Symfony\HttpFoundation\ResponseProcessorInterface;
 use Swoole\Http\Response as SwooleResponse;
 use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 use function pack;
 use function strlen;
+use function strpos;
 
 final class GrpcResponseProcessor implements ResponseProcessorInterface
 {
@@ -25,11 +26,40 @@ final class GrpcResponseProcessor implements ResponseProcessorInterface
      */
     public function process(HttpFoundationResponse $httpFoundationResponse, SwooleResponse $swooleResponse): void
     {
-        if (!$httpFoundationResponse instanceof StreamedResponse) {
-            $swooleResponse->trailer('grpc-status', '0');
+        $contentType = (string)$httpFoundationResponse->headers->get('Content-Type');
+        if (strpos($contentType, 'application/grpc') === 0) {
+            $httpFoundationResponse->setStatusCode(200);
+
             $content = $httpFoundationResponse->getContent();
             $content = pack('CN', 0, strlen($content)).$content;
             $httpFoundationResponse->setContent($content);
+
+            // TODO: we should probably extend Symfony's Response class
+            //       and proper trailers to it instead of messing with headers
+            if ($httpFoundationResponse->headers->has('Grpc-Message')) {
+                $value = $httpFoundationResponse->headers->get('Grpc-Message', '');
+                $httpFoundationResponse->headers->remove('Grpc-Message');
+                $swooleResponse->trailer('grpc-message', $value);
+            }
+
+            try {
+                if ($httpFoundationResponse->headers->has('Grpc-Status-Details-Bin')) {
+                    $value = $httpFoundationResponse->headers->get('Grpc-Status-Details-Bin', '');
+                    $httpFoundationResponse->headers->remove('Grpc-Status-Details-Bin');
+                    $swooleResponse->trailer('Grpc-Status-Details-Bin', $value);
+                }
+            } catch (ErrorException $e) {
+                // Setting too long value throws an exception. It's not so critical, let's ignore it for now.
+                // TODO: figure out how to increase max header/trailer size
+            }
+
+            if ($httpFoundationResponse->headers->has('Grpc-Status')) {
+                $value = $httpFoundationResponse->headers->get('Grpc-Status', '');
+                $httpFoundationResponse->headers->remove('Grpc-Status');
+                $swooleResponse->trailer('Grpc-Status', $value);
+            } else {
+                $swooleResponse->trailer('Grpc-Status', '0');
+            }
         }
 
         $this->decorated->process($httpFoundationResponse, $swooleResponse);
